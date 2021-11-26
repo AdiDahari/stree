1419f11a72424dda3ecba2f0fc514e41af8b4b3c
AdiDahari <dahariadi16@gmail.com>
2021-11-26 19:18:20 +0200

    almost done, indent

--- stree.c
+++ stree.c
@@ -8,32 +8,29 @@
 #include <grp.h>
 #include <dirent.h>
 
+typedef struct node
+{
+    int level, last, indent;
+    struct node *prev;
+    struct node *next;
+} node;
+
+node *head;
+node *curr;
 char *ENTRY = "├── ";
 char *LAST = "└── ";
-int n_files = 0, n_dirs = 0, prefix = 0;
+int n_files = 0, n_dirs = 0, prefix = 0, max_lvl = 0;
 char *perms[] = {"---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"};
 
 static int list(const char *name, const struct stat *status, int type, struct FTW *ftwb)
 {
-    if (name[ftwb->base] == '.')
-        return 0;
-
-    for (int i = prefix; i < strlen(name) - 1; ++i)
-    {
-        if (name[i] == '/' && name[i + 1] == '.')
-        {
-            return 0;
-        }
-    }
     if (type == FTW_NS)
         return 0;
 
     if (type == FTW_F)
     {
-        for (int i = 1; i < ftwb->level; ++i)
-        {
-            printf("│   ");
-        }
+        curr = curr->next;
+
         struct passwd *pwd;
         struct group *grp;
         pwd = getpwuid(status->st_uid);
@@ -41,15 +38,41 @@ static int list(const char *name, const struct stat *status, int type, struct FT
         char a[4];
         sprintf(a, "%3o", status->st_mode & 0777);
         ++n_files;
-        printf("%s[-%s%s%s %s %s %*ld]  %s\n", ENTRY, perms[a[0] - '0'], perms[a[1] - '0'], perms[a[2] - '0'], pwd->pw_name, grp->gr_name, 11, status->st_size, name + ftwb->base);
+        if (curr->next == NULL)
+        {
+            curr->last = 1;
+        }
+        else
+        {
+            node *ptr = curr->next;
+            while (ptr->level > curr->level && ptr->next != NULL)
+            {
+                if (ptr->next->next == NULL)
+                    curr->last = 1;
+
+                ptr = ptr->next;
+            }
+            if (ptr->level < curr->level)
+            {
+                curr->last = 1;
+            }
+        }
+        for (int i = 1; i < ftwb->level - curr->indent; ++i)
+        {
+            printf("│   ");
+        }
+        for (int i = ftwb->level - curr->indent; i < ftwb->level; ++i)
+        {
+            printf("    ");
+        }
+        printf("%s[-%s%s%s %s %s %*ld]  %s", curr->last ? LAST : ENTRY, perms[a[0] - '0'], perms[a[1] - '0'], perms[a[2] - '0'], pwd->pw_name, grp->gr_name, 11, status->st_size, name + ftwb->base);
+        printf(" (indent: %d)\n", curr->indent);
     }
 
     if (type == FTW_D && strcmp(".", name) != 0 && strcmp("", name) != 0 && strcmp("..", name) != 0)
     {
-        for (int i = 1; i < ftwb->level; ++i)
-        {
-            printf("│   ");
-        }
+        curr = curr->next;
+
         struct passwd *pwd;
         struct group *grp;
         pwd = getpwuid(status->st_uid);
@@ -57,24 +80,93 @@ static int list(const char *name, const struct stat *status, int type, struct FT
         char a[4];
         sprintf(a, "%3o", status->st_mode & 0777);
         ++n_dirs;
-        printf("%s[d%s%s%s %s %s %*ld]  %s\n", ENTRY, perms[a[0] - '0'], perms[a[1] - '0'], perms[a[2] - '0'], pwd->pw_name, grp->gr_name, 11, status->st_size, name + ftwb->base);
+        if (curr->next == NULL)
+        {
+            curr->last = 1;
+        }
+        else
+        {
+            node *ptr = curr->next;
+            while (ptr->level > curr->level && ptr->next != NULL)
+            {
+                if (ptr->next->next == NULL && ptr->next->level > curr->level)
+                {
+                    curr->last = 1;
+                }
+                ptr = ptr->next;
+            }
+            if (ptr->level < curr->level)
+            {
+                curr->last = 1;
+                ptr = curr->next;
+                if (ptr->indent == 0)
+                {
+                    while (ptr->level > curr->level && ptr->next != NULL)
+                    {
+                        ptr->indent = curr->indent + (ptr->level - curr->level);
+                        ptr = ptr->next;
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < ftwb->level - curr->indent; ++i)
+        {
+            printf("│   ");
+        }
+        for (int i = ftwb->level - curr->indent; i < ftwb->level; ++i)
+        {
+            printf("    ");
+        }
+        printf("%s[d%s%s%s %s %s %*ld]  %s", curr->last ? LAST : ENTRY, perms[a[0] - '0'], perms[a[1] - '0'], perms[a[2] - '0'], pwd->pw_name, grp->gr_name, 11, status->st_size, name + ftwb->base);
+        printf(" (indent: %d)\n", curr->indent);
     }
 
     return 0;
 }
 
+static int pre_list(const char *name, const struct stat *status, int type, struct FTW *ftwb)
+{
+    if (type == FTW_NS)
+        return 0;
+    if ((type == FTW_D && strcmp(".", name) != 0 && strcmp("", name) != 0 && strcmp("..", name) != 0) || type == FTW_F)
+    {
+        curr->next = (node *)malloc(sizeof(node));
+        curr->next->level = ftwb->level;
+        curr->next->last = curr->next->indent = 0;
+        curr->next->prev = curr;
+        curr = curr->next;
+        if (ftwb->level > max_lvl)
+            max_lvl = ftwb->level;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
+    head = (node *)malloc(sizeof(node));
+    head->level = head->last = head->indent = 0;
+    curr = head;
 
     if (argc == 1)
     {
+        nftw(".", pre_list, 1, 0);
+        curr = head;
         nftw(".", list, 1, 0);
     }
     else
     {
         prefix = strlen(argv[1]);
+        nftw(argv[1], pre_list, 1, 0);
+        curr = head;
         nftw(argv[1], list, 1, 0);
     }
+    while (head != NULL && head->next != NULL)
+    {
+        curr = head;
+        head = head->next;
+        free(curr);
+    }
+    free(head);
     printf("\n\n%d directories, %d files\n", n_dirs, n_files);
     return 0;
 }
